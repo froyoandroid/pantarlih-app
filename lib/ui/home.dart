adf958dbe41f6b1da4bbbed6a6cbe83b78f55eda
firenza <masudtxg@gmail.com>
2026-09-19 20:57:23 +0000

    feat(ui): hapus kartu Pengaturan chip keterangan dari beranda

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -5,7 +5,6 @@ import 'common.dart';
 import 'history_screen.dart';
 import 'journal_screen.dart';
 import 'lokasi_screen.dart';
-import 'pengaturan_screen.dart';
 import 'referensi_screen.dart';
 import 'rt_list_screen.dart';
 import 'search_screen.dart';
@@ -257,12 +256,6 @@ class _HomeScreenState extends State<HomeScreen> {
                       'Referensi',
                       _detailReferensi,
                       () => _open(ReferensiScreen(session: widget.session))
-                    ),
-                    (
-                      Icons.settings_outlined,
-                      'Pengaturan',
-                      'Chip keterangan warga',
-                      () => _open(PengaturanScreen(session: widget.session))
                     ),
                     (
                       Icons.admin_panel_settings_outlined,
